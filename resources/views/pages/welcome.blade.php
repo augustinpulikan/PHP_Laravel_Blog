2dd8d23b57e78dde20c127a51dcc33426a1ad7e1
Augustin Pulikan <augustinpulikan@gmail.com>
2017-10-07 19:16:10 +0530

    Modifying welcome page with recent post

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -13,24 +13,18 @@
   
   <div class="row">
       <div class="col-md-8">
-         <div class="post">
-             <h3>Post title</h3>
-             <p>the content of post will display here he content of post will display herehe content of post will display herehe content of post will display here</p>
-             <a href="#" class=" btn btn-primary">Read more</a>
-         </div>
-         <hr>
-          <div class="post">
-             <h3>Post title</h3>
-             <p>the content of post will display here he content of post will display herehe content of post will display herehe content of post will display here</p>
-             <a href="#" class=" btn btn-primary">Read more</a>
-         </div>
-          <hr>
-          <div class="post">
-             <h3>Post title</h3>
-             <p>the content of post will display here he content of post will display herehe content of post will display herehe content of post will display here</p>
-             <a href="#" class=" btn btn-primary">Read more</a>
-         </div>
-          <hr>
+
+        @foreach($posts as $post)
+
+           <div class="post">
+               <h3>{{$post->title}}</h3>
+               <p>{{substr($post->body, 0, 200)}}{{strlen($post->body)> 200 ? "..." : ""}}</p>
+               <a href="#" class=" btn btn-primary">Read more</a>
+           </div>
+           <hr>
+
+         @endforeach
+
       </div>
       <div class="col-md-3 col-md-offset-1">
       <h2>Side bar</h2>
